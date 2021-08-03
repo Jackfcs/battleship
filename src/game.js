1591eb4514d0b46403e38b31517ab44c39de44fa
JackCS <jackfcs22@gmail.com>
2021-08-03 09:10:26 +0100

    Adding ship function

--- src/game.js
+++ src/game.js
@@ -3,33 +3,64 @@ import GameBoard from "./factories/GameBoard";
 
 export const game = () => {
   let playerBoard = GameBoard();
-  let cpuBoard = GameBoard()
+  let cpuBoard = GameBoard();
   console.log(playerBoard.board);
-  playerBoard.receiveAttack(2);
-  playerBoard.receiveAttack(6);
   const playerBoardContainer = document.getElementById("player-board");
+  let cpuCells = Array.from(document.querySelectorAll(".cpu-cell"));
+  let playerCells = Array.from(document.querySelectorAll(".player-cell"));
 
-const fillBoard = (cell, board, boardArray) => {
-  let playersCells = Array.from(document.querySelectorAll(cell));
+  //Place ships on players board
+  const ship1 = Ship(2, 1)
+  const ship2 = Ship(3, 2)
+  const ship3 = Ship(4, 3)
+  const ship4 = Ship(4, 4)
+  const ship5 = Ship(5, 5)
 
-  playersCells.forEach((cell) => {
-    if (boardArray[playersCells.indexOf(cell)].hit) {
+  
+
+  const placeShip = (board, ship) => {
+    playerCells.forEach((cell) => {
+      let index = playerCells.indexOf(cell)
+      
+      cell.addEventListener("click", () => {
+        board.placeShip(ship, index)
+        // if (board.board[index].isShip){
+        //   cell.classList.remove("cell-not-hit");
+        //   cell.classList.add("cell-ship")
+        // }
+        
+        console.log(board.board)
+      })
+      updateBoard(board, index)
+    } )
+  }
+
+
+  const updateBoard = (board, index) => {
+      playerCells.forEach((cell) => {
+        if (board.board[index].isShip){
         cell.classList.remove("cell-not-hit");
-        cell.classList.add("cell-hit");
-      }
-    cell.addEventListener("click", () => {
-      board.receiveAttack(playersCells.indexOf(cell));
-      if (boardArray[playersCells.indexOf(cell)].hit) {
-        cell.classList.remove("cell-not-hit");
-        cell.classList.add("cell-hit");
-      }
+        cell.classList.add("cell-ship")
+      }  
+      })
+        
+  }
+
+  placeShip(playerBoard, ship1)
+
+  //Adds click event on each enemy cell and registers hit
+  const hitBoard = (board, boardArray) => {
+    cpuCells.forEach((cell) => {
+      let index = cpuCells.indexOf(cell)
+      cell.addEventListener("click", () => {
+        board.receiveAttack(index);
+        if (boardArray[index].hit) {
+          cell.classList.remove("cell-not-hit");
+          cell.classList.add("cell-hit");
+        }
+        
+      });
     });
-  });
-}
-
-fillBoard(".player-cell", playerBoard, playerBoard.board)
-fillBoard(".cpu-cell", cpuBoard, cpuBoard.board)
-
-
-
+  };
+  hitBoard(cpuBoard, cpuBoard.board);
 };
